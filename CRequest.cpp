62a70ee0e91d04742ba1cbd61187d46786f3dd9c
Paulina Ruszczyk <pulinka@pop-os.localdomain>
2023-09-05 01:49:50 +0200

    in case of an error, put null as quantity

--- CRequest.cpp
+++ CRequest.cpp
@@ -6,33 +6,39 @@
 #include <memory>
 #include "CRequest.h"
 
-int CRequest::retriveData(const std::string& identNumber){
-    std::array<char, 128> buffer;
-    std::string respond;
-    std::string request="curl --request GET \\\n"
-                        "  --url 'https://api.kingfisher.com/v1/mobile/stores/CAPL?filter%5Bean%5D="+identNumber+"&include=clickAndCollect%2Cstock&nearLatLong=52.172491%2C20.938261&page%5Bsize%5D=250' \\\n"
-                                                                                                                 "  --header 'Authorization: Atmosphere atmosphere_app_id=kingfisher-EPgbIZbIpBzipu0bKltAFm1xler30LKmaF4vJH96' \\\n"
-                                                                                                                 "  --cookie TS013aa2d6=011543659bd97db54671d1c15d5964995a47f65b2da76e8bde4826be936131024db94babdd19594c36ce3693a4e28e6f7f9efd33af";
+int CRequest::retriveData(const std::string& identNumber) {
+    try {
+        std::array<char, 128> buffer;
+        std::string respond;
+        std::string request = "curl --request GET \\\n"
+                              "  --url 'https://api.kingfisher.com/v1/mobile/stores/CAPL?filter%5Bean%5D=" +
+                              identNumber +
+                              "&include=clickAndCollect%2Cstock&nearLatLong=52.172491%2C20.938261&page%5Bsize%5D=250' \\\n"
+                              "  --header 'Authorization: Atmosphere atmosphere_app_id=kingfisher-EPgbIZbIpBzipu0bKltAFm1xler30LKmaF4vJH96' \\\n"
+                              "  --cookie TS013aa2d6=011543659bd97db54671d1c15d5964995a47f65b2da76e8bde4826be936131024db94babdd19594c36ce3693a4e28e6f7f9efd33af";
 
-    //std::string request="curl GET --url \"https://www.castorama.pl/cataloginventory/index/checkAvailabilityInStores?qty=1&sku="+std::to_string(identNumber)+"\"";
+        //std::string request="curl GET --url \"https://www.castorama.pl/cataloginventory/index/checkAvailabilityInStores?qty=1&sku="+std::to_string(identNumber)+"\"";
 
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(
-            popen(request.c_str(), "r"), pclose);
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(
+                popen(request.c_str(), "r"), pclose);
 
-    if (pipe)
-        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-            respond += buffer.data();
-    else
-        throw std::runtime_error("popen() failed!");
+        if (pipe && fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+            while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+                respond += buffer.data();
+        else
+            throw std::runtime_error("popen() failed!");
 
-    int quantity=0;
-    std::size_t found =respond.find("quantity");
+        int quantity = 0;
+        std::size_t found = respond.find("quantity");
 
-    while(found!=std::string::npos)
-    {
-        respond=respond.substr(found+10,respond.size());
-        quantity+=std::stoi(respond.substr(0,respond.find('}')));
-        found =respond.find("quantity");
+        while (found != std::string::npos) {
+            respond = respond.substr(found + 10, respond.size());
+            quantity += std::stoi(respond.substr(0, respond.find('}')));
+            found = respond.find("quantity");
+        }
+        return quantity;
     }
-    return quantity;
+    catch (...) {
+        return NULL;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main() {
     CRequest request;
 
-    CDataBase DB("../itemki.db");
+    CDataBase DB("/home/pulinka/Desktop/cpp/Castorama/itemki.db");
     std::vector<std::string> vIds;
 
     try {
@@ -19,28 +19,14 @@ int main() {
     }
 
     for (const auto & vId : vIds) {
-        std::string sql_insert = "INSERT INTO quantity(id, quantity, date) VALUES (" + vId + ", " +
+        std::string sql_insert;
+        if(request.retriveData(vId)!=NULL)
+            sql_insert = "INSERT INTO quantity(id, quantity, date) VALUES (" + vId + ", " +
                                  std::to_string(request.retriveData(vId)) + ", strftime ('%s', 'now'));";
+        else
+            sql_insert = "INSERT INTO quantity(id, quantity, date) VALUES (" + vId + ", NULL,  strftime ('%s', 'now'));";
+
         DB.InsertData(sql_insert.c_str());
         std::cout << "id " << vId << " quantity " << request.retriveData(vId) << std::endl;
     }
-/*
-    try{
-        document.Parse<0>(request.retriveData(vIds[0]).c_str()).HasParseError();
-        if (doc.Parse(sssss.c_str()).HasParseError() || !doc.IsObject() || !doc.HasMember("Response") || !doc["Response"].IsString())
-        {
-            return 0;
-        }
-       // document["quantity"].GetInt();
-        assert(document.HasMember("quantity"));
-        for (auto i = 0; i < document["quantity"].Size(); ++i)
-        {
-            std::cout << "port"<<i<<" = " << document["quantity"][i].GetUint() << std::endl;
-        }
-    }
-    catch(std::runtime_error & e){
-        std::cout << "Runtime error: " << e.what() << std::endl;
-    }
-*/
-
 }
